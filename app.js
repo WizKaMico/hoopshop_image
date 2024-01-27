048d2ee02e38026bd48ab66f02ec13c5ff7a5db9
revcoreitsolution@gmail.com <gmfacistol@outlook.com>
2024-01-27 13:10:49 +0800

    COMPLETE PUSH

--- app.js
+++ app.js
@@ -1,54 +1,3 @@
-// const cron = require('node-cron');
-// const bodyParser = require('body-parser');
-// const express = require('express');
-// const axios = require('axios');
-
-// const app = express();
-// const port = process.env.PORT || 3005;
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
-// const accessToken = 'EAAWiGqtpC2kBO75ZBLIm9m2PO8z52ReRncxT3TKCAPsd3yThykXMSKAET2EbiBoP7n6GEUH9RYZBlC0LKrKZA1RLe4orpINfRmctSstGffUU00x90y3xvxAsIA0MwnSP0O95yio1IBpdzuoYE9g3bOPsOMQHBN9ZA6KVac2bnT8pgZBOZCZAliIOAwoVcvdUZAzA9LEGKgV7HnskKwyv2etMDUIZD';
-// const pageId = '136968512827904';
-// const message = 'TEST NODE JS';
-// const baseAttachmentUrl = 'https://thumbs.dreamstime.com/b/michael-jordan-chicago-bulls-legend-taking-free-throw-image-taken-color-slide-73861834.jpg';
-
-// let isPostScheduled = false;
-
-// async function postToFacebook() {
-//   try {
-//     // Append timestamp to the attachment URL to ensure uniqueness
-//     const timestamp = new Date().getTime();
-//     const attachmentUrl = `${baseAttachmentUrl}?timestamp=${timestamp}`;
-
-//     // Post with the attachment and message
-//     const postResponse = await axios.post(`https://graph.facebook.com/v19.0/${pageId}/photos`, {
-//       url: attachmentUrl,
-//       caption: message,  // Using 'caption' for the message
-//       access_token: accessToken,
-//     });
-
-//     console.log('Post successful:', postResponse.data);
-//   } catch (error) {
-//     console.error('Error posting to Facebook:', error.response ? error.response.data : error.message);
-//   } finally {
-//     isPostScheduled = false;
-//   }
-// }
-
-// // Schedule the job to run every 2 minutes
-// cron.schedule('*/2 * * * *', () => {
-//   if (!isPostScheduled) {
-//     postToFacebook();
-//     isPostScheduled = true;
-//   }
-// });
-
-// // Start the server
-// app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
 const { exec } = require('child_process');
 const fs = require('fs');
 const mysql = require('mysql');
@@ -207,3 +156,4 @@ cron.schedule('*/2 * * * *', () => {
     console.error('Database connection not available.');
   }
 });
+
